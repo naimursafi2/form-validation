59391bdea4d2781e06cf413d175e7435e8bfae07
naimursafi2 <naimursafi4@gmail.com>
2025-11-25 10:48:53 +0600

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ let[email, setemail] = useState("");
 let[emailErr, setemailErr] = useState("");
 let[password, setpassword] = useState("");
 let[passwordErr, setpasswordErr] = useState("");
+// let[passwordStrong, setpasswordStrong] = useState(""); 
+
+
+
 
 const fullNameHandle = (e)=> {
   setfullName(e.target.value);
   setnameErr("");
-  
 }
 const emailHandle = (e)=> {
   setemail(e.target.value);
@@ -20,9 +23,12 @@ const emailHandle = (e)=> {
 }
 const passwordHandle = (e)=> {
   setpassword(e.target.value);
+  // passwordStrong(e.target.value);
   setpasswordErr("")
   
 }
+const passCheeck =
+/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 const clickHandle = () => {
     if(!fullName){
@@ -34,6 +40,13 @@ const clickHandle = () => {
     if(!password){
       setpasswordErr("passward invalid")
     }
+    if(!passCheeck.test(password)){
+      setpasswordErr("password week")
+    }
+    // else{
+    //   setpasswordStrong("password strong")
+    // }
+    
 }
   return (
     <div className='flex flex-col border-3 border-green-800 p-5 w-xl m-auto rounded-2xl mt-17'>
